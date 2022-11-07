8ee060777e4b89a46989acafb8c6932879e9bf93
MrKeeplearning <jaeseok1115@gmail.com>
2022-11-08 01:29:23 +0900

    fix: 기존 feature/userInfo 브랜치 오류 수정
    * feature/mentoring 브랜치 신규 생성 및 merge를 진행하며 기존 브랜치에서
      존재하던 오류 수정 및 사용하지 않는 코드 삭제

--- src/main/java/com/project/wah/projectwebservice/service/MessageService.java
+++ src/main/java/com/project/wah/projectwebservice/service/MessageService.java
@@ -13,9 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -55,30 +53,6 @@ public class MessageService {
         return new MessageReadResponseDto(message);
     }
 
-
-//    // 쪽지 삭제
-//    @Transactional
-//    public ResponseEntity<MessageDeleteResponseDto> deleteMessage(int messageId, @LoginUser SessionUser sessionUser) {
-//
-//        Optional<User> userOptional = userRepository.findById(sessionUser.getId());
-//        User user = userOptional.get();
-//
-//        boolean isValid = user.deleteMessage(messageId);
-//
-//        if (!isValid) {
-//            log.error("nickname={}, messageId={}, error={}", user.getNickname(), messageId, "해당 쪽지를 찾을 수 없음");
-//            return new ResponseEntity<>(
-//                    MessageDeleteResponseDto.builder().status(StatusMessage.BAD_REQUEST).build(),
-//                    HttpStatus.valueOf(StatusCode.BAD_REQUEST)
-//            );
-//        }
-//        messageRepository.deleteById(messageId);
-//
-//        return new ResponseEntity<>(
-//                MessageDeleteResponseDto.builder().status(StatusMessage.SUCCESS).build(),
-//                HttpStatus.valueOf(StatusCode.SUCCESS)
-//        );
-//    }
     // 받은 쪽지 리스트 조회
     @Transactional
     public Page<MessageListReadResponseDto> findAllReceiverDesc(@LoginUser SessionUser sessionUser, Pageable pageable) {
@@ -100,68 +74,4 @@ public class MessageService {
         return messagePagingList;
     }
 
-//    // 받은 쪽지 검색 후 리스트 조회
-//    @Transactional
-//    public Page<MessageListReadResponseDto> findReceiverMessageSearch(String title, Pageable pageable, @LoginUser SessionUser sessionUser) {
-//
-//        Page<Message> messageList = messageRepository.findReceiverByTitleContaining(title, pageable);
-//
-//        Page<MessageListReadResponseDto> messagePagingList = messageList.map(message -> new MessageListReadResponseDto(message));
-//
-//        return messagePagingList;
-//    }
-//
-//    // 보낸 쪽지 검색 후 리스트 조회
-//    @Transactional
-//    public Page<MessageListReadResponseDto> findSenderMessagesearch(@LoginUser SessionUser sessionUser, Pageable pageable) {
-//
-//        Page<Message> messageList = messageRepository.findSenderByTitleContaining(sessionUser.getId(), pageable);
-//
-//        Page<MessageListReadResponseDto> messagePagingList = messageList.map(message -> new MessageListReadResponseDto(message));
-//
-//        return messagePagingList;
-//    }
-
-
-
 }
-
-
-//    Optional<User> userOptional = userRepository.findById(sessionUser.getId());
-//    User sendUser = userOptional.get();
-//
-//    Optional<User> receivedUserOptional = userRepository.findByNickname(requestDto.getReceiver());
-//        if(receivedUserOptional.isEmpty()) {
-//                log.error("nickname={}, error={}", requestDto.getReceiver(), "쪽지 수신자가 존재하지 않음");
-//                return new ResponseEntity<>(
-//        MessageCreateResponseDto.builder().status(StatusMessage.UNKNOWN_RECEIVER).build(),
-//        HttpStatus.valueOf(StatusCode.BAD_REQUEST)
-//        );
-//        };
-//
-//        User receivedUser = receivedUserOptional.get();
-//
-//        // 쪽지 내용 255자 이내로 제한
-//        if (requestDto.getContent().length() > 255) {
-//        log.error("nickname={}, error={}", sendUser.getNickname(), "쪽지 내용 255자 초과");
-//        return new ResponseEntity<>(
-//        MessageCreateResponseDto.builder().status(StatusMessage.BAD_REQUEST).build(),
-//        HttpStatus.valueOf(StatusCode.BAD_REQUEST)
-//        );
-//        }
-//
-//        // 메시지 보내는 코드
-//        Message message = Message.builder()
-//        .receiver(receivedUser)
-//        .title(requestDto.getTitle())
-//        .content(requestDto.getContent())
-//        .createdate(requestDto.getCreateDate())
-//        .build();
-//
-//        sendUser.sendMessage(message);
-//        messageRepository.save(message);
-//
-//        return new ResponseEntity<>(
-//        MessageCreateResponseDto.builder().status(StatusMessage.SUCCESS).build(),
-//        HttpStatus.valueOf(StatusCode.SUCCESS)
-//        );
--- src/main/java/com/project/wah/projectwebservice/web/MessageController.java
+++ src/main/java/com/project/wah/projectwebservice/web/MessageController.java
@@ -3,20 +3,10 @@ package com.project.wah.projectwebservice.web;
 import com.project.wah.projectwebservice.config.auth.LoginUser;
 import com.project.wah.projectwebservice.config.auth.dto.SessionUser;
 import com.project.wah.projectwebservice.domain.message.Message;
-import com.project.wah.projectwebservice.domain.user.User;
-import com.project.wah.projectwebservice.domain.user.UserRepository;
-import com.project.wah.projectwebservice.response.Response;
 import com.project.wah.projectwebservice.service.MessageService;
-import com.project.wah.projectwebservice.web.dto.MessageDto;
 import com.project.wah.projectwebservice.web.dto.message.*;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -37,12 +27,6 @@ public class MessageController {
         return messageService.findById(messageId);
     }
 
-
-//    @DeleteMapping("/message/{messageId}")
-//    public ResponseEntity<MessageDeleteResponseDto> find(@PathVariable int messageId,
-//                                                         @LoginUser SessionUser sessionUser) {
-//        return messageService.deleteMessage(messageId, sessionUser);
-//    }
 }
 
 

6ea8220ee2f0353fdd6bd00a2182405c618b12c9
MrKeeplearning <jaeseok1115@gmail.com>
2022-11-28 17:38:06 +0900

    ci: 배포 자동화 구성

--- src/main/java/com/project/wah/projectwebservice/ProjectWebserviceApplication.java
+++ src/main/java/com/project/wah/projectwebservice/ProjectWebserviceApplication.java
@@ -2,14 +2,15 @@ package com.project.wah.projectwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class ProjectWebserviceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectWebserviceApplication.class, args);
+		SpringApplication application = new SpringApplication(ProjectWebserviceApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
-
 }
